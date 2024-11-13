8916acf29aaf7e3ca862783fa7b95b703b624ba3
SSOFERRET <owosso1212@gmail.com>
2024-11-13 12:18:27 +0900

    SSR 실습

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,17 +4,47 @@ import style from "./index.module.css";
 import { ReactNode } from "react";
 import books from "@/mock/books.json";
 import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
+import fetchRandomBooks from "@/lib/fetch-random-books";
 
-export default function Home() {
+// 약속된 이름의 함수를 만들어서 내보내주면 SSR로 동작하도록 설정됨
+export const getServerSideProps = async() => {
+  //컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수.
+  // 이 함수는 서버 측에서만 실행 됨
+  // console.log("서버사이드프롭스")
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(), 
+    fetchRandomBooks(),
+  ]);
+
+  return {
+    props: {
+      allBooks,
+      recoBooks,
+    }
+  }
+};
+
+//서버에서 1번, 브라우저에서 1번 실행됨 (사전 렌더링, 하이드레이션 과정)
+export default function Home({ allBooks, recoBooks }: InferGetServerSidePropsType<typeof getServerSideProps>) {
+  // InferGetServerSidePropsType: SSR로 받은 데이터의 타입을 추론해주는 Nextjs 내장 타입 
+
+  //window.location < 서버사이드에서는 window 객체가 없으므로 오류 발생함.
+  //window 객체를 사용하고 싶다면 아래처럼 useEffect를 사용해야함.
+  // useEffect(() => {
+  //   console.log(window)
+  // }, [])
+
   return (
     <div>
       <section className={style.container}>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {recoBooks.map((book) => <BookItem key={book.id} {...book} />)}
       </section>
       <section className={style.container}>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => <BookItem key={book.id} {...book} />)}
+        {allBooks.map((book) => <BookItem key={book.id} {...book} />)}
         </section>
     </div>
   );
--- src/pages/search/index.tsx
+++ src/pages/search/index.tsx
@@ -1,9 +1,22 @@
 import BookItem from "@/components/book-item";
 import SearchableLayout from "@/components/searchable-layout";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
+import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import fetchBooks from "@/lib/fetch-books";
 
-export default function Page() {
+// context 매개변수에는 현재 브라우저로부터 받은 요청의 모든 요청이 담기게 된다.
+export const getServerSideProps = async (context: GetServerSidePropsContext) => {
+    const q = context.query.q;
+    const books = await fetchBooks(q as string);
+    
+    return {
+        props: {
+            books
+        }
+    }
+}
+
+export default function Page({ books }: InferGetServerSidePropsType<typeof getServerSideProps>) {
     return <div>
         {books.map((book) => <BookItem key={book.id} {...book} />)}
     </div>
